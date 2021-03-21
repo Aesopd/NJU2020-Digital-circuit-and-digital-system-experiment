ba58d003d2288b1e4786a8d81e4683b1d8d13fb9
Aesopd <70428833+Aesopd@users.noreply.github.com>
2021-03-21 16:11:08 +0800

    Add files via upload

---
+++ exp04/exp_four_one/obstructive/obstructive.v
@@ -0,0 +1,17 @@
+module obstructive(clk,in_en,out_lock1,in_data,out_lock2);
+input clk;
+input in_en;
+input in_data;
+output reg out_lock1,out_lock2;
+always@(posedge clk)
+	if(in_en)
+		begin
+			out_lock1=in_data;
+			out_lock2=out_lock1;
+		end
+	else
+		begin
+			out_lock1=out_lock1;
+			out_lock2=out_lock2;
+		end
+endmodule
